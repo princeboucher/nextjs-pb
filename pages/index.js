6b658a2b3fe422356d6059fdaa460c6d59040188
princeboucher <736187+princeboucher@users.noreply.github.com>
2022-05-22 00:23:21 -0700

    updated senence
    updated sentence

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const Index = () => (
   <div>
-    Hello World.{' '}
+    Hello, world. This is my first next.js site. I'm building it because I'm trying to sharpen my web3 skills and read from some tweets that this might be a solid route.{' '}
     <Link href="/about">
       <a>About</a>
     </Link>
